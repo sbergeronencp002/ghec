ca707283e8187a667d1ee354eebc4d5ba467c291
sbergeronencp002 <sbergeron.ens@gmail.com>
2026-07-23 09:11:09 -0400

    Ajouter Q2 — Établir des faits (Les Algonquiens vers 1500) [index]

--- questions-index.js
+++ questions-index.js
@@ -1,3 +1,4 @@
 const QUESTIONS = [
-{id: "Q1", niveau: 3, oi: "Établir des faits", competence: "Lire l'organisation du territoire", periodes: ["Les Iroquoiens vers 1500"], points: 1, soustag: "Établir 1 fait - 1", aspects: [{aspect: "Territoire"}], enonce: "Nomme le mode de vie des Iroquoiens vers 1500.", updatedAt: "2026-07-23T13:02:04.859Z"}
+{id: "Q1", niveau: 3, oi: "Établir des faits", competence: "Lire l'organisation du territoire", periodes: ["Les Iroquoiens vers 1500"], points: 1, soustag: "Établir 1 fait - 1", aspects: [{aspect: "Territoire"}], enonce: "Nomme le mode de vie des Iroquoiens vers 1500.", updatedAt: "2026-07-23T13:02:04.859Z"},
+{id: "Q2", niveau: 3, oi: "Établir des faits", competence: "Lire l'organisation du territoire", periodes: ["Les Algonquiens vers 1500"], points: 1, soustag: "Établir 1 fait - 1", aspects: [{aspect: "Territoire"}], enonce: "Nomme le mode de vie des Algonquiens vers 1500.", updatedAt: "2026-07-23T13:11:07.174Z"}
 ]

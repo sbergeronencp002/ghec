d3dc980a01841337202cbd9819f468715cad2594
sbergeronencp002 <sbergeron.ens@gmail.com>
2026-07-23 09:02:07 -0400

    Ajouter Q1 — Établir des faits (Les Iroquoiens vers 1500) [index]

--- questions-index.js
+++ questions-index.js
@@ -1,3 +1,3 @@
 const QUESTIONS = [
-
+{id: "Q1", niveau: 3, oi: "Établir des faits", competence: "Lire l'organisation du territoire", periodes: ["Les Iroquoiens vers 1500"], points: 1, soustag: "Établir 1 fait - 1", aspects: [{aspect: "Territoire"}], enonce: "Nomme le mode de vie des Iroquoiens vers 1500.", updatedAt: "2026-07-23T13:02:04.859Z"}
 ]

904e16200daff2a37035f970dea123e2c17049bb
sbergeronencp002 <sbergeron.ens@gmail.com>
2026-08-14 08:36:25 -0400

    Ajouter Q3 — Établir des faits (Les Iroquoiens vers 1500) [index]

--- questions-index.js
+++ questions-index.js
@@ -1,4 +1,5 @@
 const QUESTIONS = [
 {id: "Q1", niveau: 3, oi: "Établir des faits", competence: "Lire l'organisation du territoire", periodes: ["Les Iroquoiens vers 1500"], points: 1, soustag: "Établir 1 fait - 1", aspects: [{aspect: "Population"}], enonce: "Nomme le mode de vie des Iroquoiens vers 1500.", updatedAt: "2026-08-14T12:35:01.718Z"},
-{id: "Q2", niveau: 3, oi: "Établir des faits", competence: "Lire l'organisation du territoire", periodes: ["Les Algonquiens vers 1500"], points: 1, soustag: "Établir 1 fait - 1", aspects: [{aspect: "Personnages"}], enonce: "Nomme le mode de vie des Algonquiens vers 1500.", updatedAt: "2026-08-14T12:35:58.339Z"}
+{id: "Q2", niveau: 3, oi: "Établir des faits", competence: "Lire l'organisation du territoire", periodes: ["Les Algonquiens vers 1500"], points: 1, soustag: "Établir 1 fait - 1", aspects: [{aspect: "Personnages"}], enonce: "Nomme le mode de vie des Algonquiens vers 1500.", updatedAt: "2026-08-14T12:35:58.339Z"},
+{id: "Q3", niveau: 3, oi: "Établir des faits", competence: "Lire l'organisation du territoire", periodes: ["Les Iroquoiens vers 1500"], points: 1, soustag: "Établir 1 fait - 1", aspects: [{aspect: "Activités économiques"}], enonce: "Nomme une activité pratiquée par les Iroquoiens pour se nourrir.", updatedAt: "2026-08-14T12:36:25.008Z"}
 ]
